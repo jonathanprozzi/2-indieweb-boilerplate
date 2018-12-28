a000776531af2afb476b813654db7b5da93f2f58
jonathanprozzi <jonathanprozzi@gmail.com>
2018-12-28 14:10:54 -0500

    added basic postLayout component and tested it by generating a fake page in gatsby-node

---
+++ src/components/postLayout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql, Link } from "gatsby";
+import Layout from "./layout";
+
+const PostLayout = ({ props }) => (
+  <div>
+    <h1>Post Layout Test</h1>
+  </div>
+);
+
+export default PostLayout;
