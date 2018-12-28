6fc16f8302f53890790ea58463703cc1b7a2118f
jonathanprozzi <jonathanprozzi@gmail.com>
2018-12-28 00:14:41 -0500

    added more to the config

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from './header'
-import './layout.css'
+import Header from "./header";
+import "./layout.css";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -13,10 +13,6 @@ const Layout = ({ children }) => (
           siteMetadata {
             title
             description
-            tagline
-            author
-            position
-            organizaiton
           }
         }
       }
@@ -29,7 +25,7 @@ const Layout = ({ children }) => (
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           {children}
@@ -40,10 +36,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
